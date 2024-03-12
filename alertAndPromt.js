3b50573b7cecf79d15f56f7cd2b65c0e285ae4e3
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 13:26:41 +0530

    prompts and alerts in javascript

---
+++ alertAndPromt.js
@@ -0,0 +1,6 @@
+alert("i love js more than anything it is the fuctional lanquege thats why i just love this lanquage")
+let number = prompt("enter a nuber")
+alert("your number is "+number)
+
+let firstName=prompt("Enter your first name");
+alert(`Your name is ${firstName}`);
