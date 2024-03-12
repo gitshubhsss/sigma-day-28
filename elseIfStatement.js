ccb2a08645769c84ea9feec3269c19e5c978fc54
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 11:59:54 +0530

    else if statements in JS

---
+++ elseIfStatement.js
@@ -0,0 +1,12 @@
+let age=18;
+
+if(age>=18){
+    console.log("you can drive and vote ");
+}
+else if(age<18 ) {
+    console.log("You cannot drive and vote");
+}
+
+else{
+    console.log("you can do anything");
+}
