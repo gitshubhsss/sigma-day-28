1bdeb47e57864305959cec66cf71153661782da4
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 12:11:53 +0530

    practice question

---
+++ practiceQ1.js
@@ -0,0 +1,17 @@
+let size="M";
+
+
+if(size=="XL"){
+    console.log("Price is 250");
+}
+else if(size=="L"){
+    console.log("Price is 200");
+}
+else if(size=="M"){
+    console.log("Price is 100");
+}
+else if(size=="S"){
+    console.log("Price is 50");
+}
+
+
