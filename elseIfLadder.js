c9c4d5e06e843bc892e5a61c8e4b026d1fdfccff
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 12:27:30 +0530

    elseIfLadder in js

---
+++ elseIfLadder.js
@@ -0,0 +1,15 @@
+let marks=80;
+
+if(marks>=33){
+    console.log("PASS");
+    if(marks>=80)
+    {
+        console.log("O grade");
+    }
+    else{
+        console.log("A grade");
+    }
+}
+else{
+    console.log("better luck next time");
+}
