6633741e928f221536cd6da362c98adb012f80cb
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 13:35:45 +0530

    practice quetion 5

---
+++ practiceQ5.js
@@ -0,0 +1,6 @@
+// aketheuser'sname&ageasinputusingprompts.Thenreturnbackthefollowingstatementtotheuserasanalert(bysubstitutingtheirname&age):nameisageyearsold.
+
+let userName=prompt("Enter your name");
+let age=prompt("Enter your age");
+
+alert(`${userName} is ${age} years old`)
