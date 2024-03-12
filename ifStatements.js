e35db3eaffe5dddb7bdd9bd14f957853463a13bd
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 11:53:42 +0530

    conditional statements in javascript

---
+++ ifStatements.js
@@ -0,0 +1,14 @@
+// create a traffic light program using if statements
+let color= "red";
+color= "green";
+color= "yellow";
+
+if(color="red"){
+    console.log("stop");
+}
+if(color="green"){
+    console.log("you can go");
+}
+if(color="yellow"){
+    console.log("drive alertly");
+}
