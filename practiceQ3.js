bc18cdbd13eb476e40b3fcbad36888cbe7a8b68e
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-12 13:09:59 +0530

    Switch statements in js

---
+++ practiceQ3.js
@@ -0,0 +1,33 @@
+//use the switch statements to print the day of the week using a number variable 'day' with values 0 to 7
+
+let dayNumber = 7;
+
+switch (dayNumber) {
+  case 1:
+    console.log("monday");
+    break;
+
+  case 2:
+    console.log("tuesday");
+    break;
+
+  case 3:
+    console.log("Wednesday");
+    break;
+
+  case 4:
+    console.log("thursday");
+    break;
+
+  case 5:
+    console.log("friday");
+    break;
+
+  case 6:
+    console.log("saturday");
+    break;
+
+  case 7:
+    console.log("sunday");
+    break;
+}
